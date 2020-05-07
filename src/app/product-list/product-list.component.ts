a66fc7ff16a8af3a11df18eb15da16727373cf52
KavehKarami <kaveh.karami.kk@gmail.com>
2020-05-07 11:05:05 +0430

    a simple app

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,8 +10,12 @@ import { products } from '../products';
 export class ProductListComponent {
   products = products;
 
-  share() {
-    window.alert('The product has been shared!');
+  share(name) {
+    window.alert(`The ${name} has been shared!`);
+  }
+
+  onNotify(phoneModel) {
+    window.alert(`You will be notified when the ${phoneModel} goes on sale`);
   }
 }
 
